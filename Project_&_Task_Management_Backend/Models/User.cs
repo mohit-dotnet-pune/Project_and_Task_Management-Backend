3c1996d0c6bcc6f91cc679d5d4ee087d8f7aff5c
mohit-dotnet-pune <mohitmabagul@gmail.com>
2025-11-26 15:38:43 +0530

    added dependencies

--- Project_&_Task_Management_Backend/Models/User.cs
+++ Project_&_Task_Management_Backend/Models/User.cs
@@ -2,5 +2,10 @@
 {
     public class User
     {
+        public int UserId {  get; set; }
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
     }
 }
