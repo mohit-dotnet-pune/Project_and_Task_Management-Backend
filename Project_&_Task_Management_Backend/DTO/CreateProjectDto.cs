c070eb02dc0edf0b587cc7467479ae34768e3403
Vaibhav.Indure <indurevaibhav9@gmail.com>
2025-11-27 15:52:37 +0530

    cascade delete done and pulled

---
+++ Project_&_Task_Management_Backend/DTO/CreateProjectDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Project___Task_Management_Backend.DTO
+{
+    public class CreateProjectDto
+    {
+        [Required]
+        public string projectName { get; set; }
+        [Required]
+        public string projectDescription { get; set; }
+        [Required]
+        public DateTime projectStartDate { get; set; }
+        [Required]
+        public DateTime projectEndDate { get; set; }
+    }
+}
